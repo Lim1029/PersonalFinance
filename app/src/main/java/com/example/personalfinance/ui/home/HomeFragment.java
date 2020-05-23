cff779593e215b01f260fde98d835bf4fc39de03
Lim1029 <mk1029@hotmail.com>
2020-05-23 07:04:30 -0700

    Experiment with Home page gridview with custom adapter with hard-coded Money array

--- app/src/main/java/com/example/personalfinance/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/personalfinance/ui/home/HomeFragment.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -30,6 +32,11 @@ public class HomeFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
+        GridView gv = root.findViewById(R.id.homeGrid);
+        gv.setAdapter(new HomeGridAdapter(getContext()));
         return root;
+
+
     }
 }
