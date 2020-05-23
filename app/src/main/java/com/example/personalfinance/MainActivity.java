5a99f44d48d32a7276afb6c807dab0fb7b3b732b
Lim1029 <mk1029@hotmail.com>
2020-05-23 16:45:15 +0800

    Add parse sdk

--- app/src/main/java/com/example/personalfinance/MainActivity.java
+++ app/src/main/java/com/example/personalfinance/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.personalfinance;
 import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.parse.ParseInstallation;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -17,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
+        ParseInstallation.getCurrentInstallation().saveInBackground();
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
@@ -25,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
+
     }
 
 }
