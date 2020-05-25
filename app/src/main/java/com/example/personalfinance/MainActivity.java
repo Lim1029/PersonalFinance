c3843f9731594ef8b4bee31396409c2c6392d252
zhanhong-spec <limhong2001.gmail.com>
2020-05-25 21:30:40 +0800

    added user signup/login activity

--- app/src/main/java/com/example/personalfinance/MainActivity.java
+++ app/src/main/java/com/example/personalfinance/MainActivity.java
@@ -1,10 +1,18 @@
 package com.example.personalfinance;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.parse.LogOutCallback;
+import com.parse.ParseException;
 import com.parse.ParseInstallation;
+import com.parse.ParseUser;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -28,6 +36,32 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
 
-        // nothing feiafoja
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_logout, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if(item.getItemId() == R.id.userLogoutItem) {
+            ParseUser.logOutInBackground(new LogOutCallback() {
+                @Override
+                public void done(ParseException e) {
+                    if(e == null) {
+                        Toast.makeText(MainActivity.this,"You have logged out successfully", Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+                }
+            });
+            Intent intent = new Intent(MainActivity.this, Signup_Login.class);
+            finish();
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
